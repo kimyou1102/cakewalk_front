21300d1de837a5170874a6153a8b8337f6cb57c1
Sun Ho <tjsgh5312@daum.net>
2022-08-06 22:46:39 +0900

    img 사이즈 고정이 안된다 ㅜㅜㅜ

--- main.js
+++ main.js
@@ -22,16 +22,7 @@ canvas.setDimensions({width : `${canvas_width}`, height: `${canvas_height}`});
 canvas2.setDimensions({width : `${canvas2_width}`, height: `${canvas2_height}`});
 
 window.addEventListener('resize',()=>{
-    canvas_width = workBoard.clientWidth- 48;
-    canvas_height = workBoard.clientHeight - 64;
-    
-    canvas2_width = workBoard2.clientWidth - 24;
-    canvas2_height = workBoard2.clientHeight - 40;
-    
-    canvas.setDimensions({width : `${canvas_width}`, height: `${canvas_height}`});
-    canvas2.setDimensions({width : `${canvas2_width}`, height: `${canvas2_height}`});
-
-    drawShape(canvas, cur_shape);
+    resizeCanvasSize();
 });
 
 shape_btns.forEach( (ele) => {
@@ -53,11 +44,7 @@ function drawShape(canvas, shape){
         return;
     }
     else{
-        let side_posX = canvas2_width /2 - (canvas2_width * mag/3);
-        let side_posY = canvas2_height /2 -(canvas2_height * mag/6);
         let sideRect = new fabric.Rect({
-            top: side_posY,
-            left : side_posX,
             width: canvas2_width * 2 * mag/3 ,
             height : canvas2_height * mag/3,
             fill:'#ccc',
@@ -66,109 +53,90 @@ function drawShape(canvas, shape){
         });
         sideRect.set('selectable', false);
         canvas2.add(sideRect);
+        canvas2.centerObject(sideRect);
     }
     let posX = 0;
     let posY = 0;
     switch(shape){
         case 'circle':
-            posY = (canvas_height /2) - (canvas_width * mag/4);
-            posX = (canvas_width / 2) - (canvas_width * mag/4);
             let circle = new fabric.Circle({
-                top: posY,
-                left : posX,
                 radius : canvas_width * mag /4,
                 stroke: 'black',
                 strokeWidth : 3,
                 fill: '#ccc'
             });
-            circle.set('selectable', false);
+            //circle.set('selectable', false);
             canvas.add(circle);
+            canvas.centerObject(circle);
             break;
         case 'rectangle':
-            posY = (canvas_height / 2) - (canvas_width * mag/4);
-            posX = (canvas_width / 2) - (canvas_width * mag/4);
             let rect = new fabric.Rect({
-                top: posY,
-                left: posX,
                 width: canvas_width * mag/2,
                 height: canvas_width * mag/2,
                 stroke: 'black',
                 strokeWidth : 3,
                 fill: '#ccc'
             });
-            rect.set('selectable', false);
+            //rect.set('selectable', false);
             canvas.add(rect);
+            canvas.centerObject(rect);
             break;
         case 'triangle':
-            posY = canvas_height / 2 -((canvas_width / 2) * Math.sqrt(3) * mag / 4);
-            posX = canvas_width / 4;
             let tri = new fabric.Triangle({
-                top: posY,
-                left : posX,
                 width : canvas_width * mag/ 2,
                 height: (canvas_width / 2) * Math.sqrt(3)* mag / 2,
                 stroke: 'black',
                 strokeWidth : 3,
                 fill: '#ccc'
             });
-            tri.set('selectable', false);
+            //tri.set('selectable', false);
             canvas.add(tri);
+            canvas.centerObject(tri);
             break;
         case 'star':
-            posY = canvas_height / 2 -(canvas_width * mag/3);
-            posX = canvas_width / 2 -(canvas_width * mag/ 3); 
             let star = new fabric.Text('★',{
-                top: posY,
-                left : posX,
                 fontSize: canvas_width * 2 * mag / 3,
                 stroke: 'black',
                 strokeWidth : 3,
                 fill: '#ccc'
             });
-            star.set('selectable', false);
+            //star.set('selectable', false);
             canvas.add(star);
+            canvas.centerObject(star);
             break;
         case 'heart':
-            posY = canvas_height / 2 - (canvas_width * 3 * mag / 8);
-            posX = canvas_width / 2 -(canvas_width * mag / 5); 
             let heart = new fabric.Text('♥',{
-                top: posY,
-                left : posX,
                 fontSize: canvas_width * 3 * mag /4,
                 stroke: 'black',
                 strokeWidth : 3,
                 fill: '#ccc'
             });
-            heart.set('selectable', false);
+            //heart.set('selectable', false);
             canvas.add(heart);
+            canvas.centerObject(heart);
             break;
         case 'dubble':
-            posY = canvas_height / 2 - (canvas_width * mag / 4);
-            posX = canvas_width / 2 - (canvas_width * mag / 4);
             let first_circle = new fabric.Circle({
-                top: posY,
-                left : posX,
                 radius : canvas_width * mag/ 4,
                 stroke: 'black',
                 strokeWidth : 3,
                 fill: '#ccc'
             });
-            let posY2 = canvas_height / 2 - (canvas_width * mag / 10);
-            let posX2 = canvas_width / 2 - (canvas_width * mag / 10);
             let second_circle = new fabric.Circle({
-                top: posY2,
-                left : posX2,
                 radius : canvas_width * mag / 10,
                 stroke: 'black',
                 strokeWidth : 3,
                 fill: 'white'
             }); 
+            canvas.centerObject(first_circle);
+            canvas.centerObject(second_circle);
             let group = new fabric.Group([first_circle, second_circle]);
-            group.set('selectable', false);
+            //group.set('selectable', false);
             canvas.add(group);
     }
 }
 
+/*--------------------------------- Board Switch --------------------------------- */
 const switch_btn = document.querySelector('.board_switch_btn');
 const work_board = document.querySelector('.work_board');
 const second_work_board = document.querySelector('.second_work_board');
@@ -191,8 +159,14 @@ function resizeCanvasSize(){
 
     canvas.setDimensions({width : `${canvas_width}`, height: `${canvas_height}`});
     canvas2.setDimensions({width : `${canvas2_width}`, height: `${canvas2_height}`});
-
-    drawShape(canvas , cur_shape);
+    
+    if(switch_choice == 'shape'){
+        drawShape(canvas , cur_shape);
+    }
+    else{
+        drawImage();
+    }
+    
 }
 
 /*--------------------------------- Memo --------------------------------- */
@@ -278,7 +252,12 @@ size_radios.forEach(element => {
                 default:
                     break;
             }
-            drawShape(canvas, cur_shape);
+            if(switch_choice == 'shape'){
+                drawShape(canvas, cur_shape);
+            }
+            else{
+                drawImage();
+            }
         }
     });
 });
@@ -287,20 +266,33 @@ size_radios.forEach(element => {
 const shape_switch = document.querySelector('.shape_switch');
 const shape_switch_items = document.querySelectorAll('.shape_switch_item');
 const select_box = document.querySelector('.select_box');
+
+const shape = document.querySelector('.shape');
+const picture = document.querySelector('.picture');
+
+let switch_choice = 'shape';
+
 moveSwitch();
 
 function moveSwitch(){
     shape_switch.addEventListener('click',()=>{
+        shape.classList.toggle('unactive');
+        picture.classList.toggle('unactive');
+        
         shape_switch_items.forEach(element => {
             element.classList.toggle('shape_switch_item_active');
 
+            //사진 첨부 active
             if (element.classList.contains('shape_switch_item_active')){
                 select_box.style.left='auto';
-                select_box.style.right = 0;  
+                select_box.style.right = 0;
+                switch_choice = 'pic';
             }
+            //기본 모양 active
             else{
                 select_box.style.right = 'auto';    
                 select_box.style.left=0;
+                switch_choice = 'shape';
             }
         });    
     });
@@ -309,11 +301,90 @@ function moveSwitch(){
 /*--------------------------------- Image file --------------------------------- */
 const realUpload = document.querySelector('.real_upload');
 const upload = document.querySelector('.upload');
+let pic_url = null;
+let img_scale;
+let img_top;
+let img_left;
 
 upload.addEventListener('click', ()=> realUpload.click());
-realUpload.addEventListener('change', getImageFiles(realUpload));
+realUpload.addEventListener('change', ()=>{
+    let selected_pic = realUpload.files[0];
+    
+    pic_url = URL.createObjectURL(selected_pic);
+    drawImage();
+});
 
-function getImageFiles(e){
-    const files = e.files[0];
-    console.log(files);
+function drawImage(){
+    canvas.clear();
+    canvas2.clear();
+    if(shape == null){
+        return;
+    }
+    else{
+        let side_posX = canvas2_width /2 - (canvas2_width * mag/3);
+        let side_posY = canvas2_height /2 -(canvas2_height * mag/6);
+        let sideRect = new fabric.Rect({
+            top: side_posY,
+            left : side_posX,
+            width: canvas2_width * 2 * mag/3 ,
+            height : canvas2_height * mag/3,
+            fill:'#ccc',
+            stroke : 'black',
+            strokeWidth: 3,
+        });
+        sideRect.set('selectable', false);
+        canvas2.add(sideRect);
+    }
+
+    fabric.Image.fromURL(`${pic_url}`, function(img){
+        img.on('scaling',(option)=>{  
+            img_scale = img.scaleX;
+            img_top = img.top;
+            img_left = img.left;
+
+        });
+            
+        canvas.add(img);
+        canvas.centerObject(img)
+    });
 }
+/*--------------------------------- order Num --------------------------------- */
+const step_boxes = document.querySelectorAll('.step_box');
+let active_step_box = document.querySelector('.active_step_box');
+
+const order_num_items = document.querySelectorAll('.order_num_item');
+const order_title = document.querySelector('.order_title');
+const order_list = [
+    {order : 1, title:"시트 모양 정하기", color:"#F8BBD0"},
+    {order : 2, title:"케이크 색 정하기", color:"#F06292"},
+    {order : 3, title:"레터링", color:"#EC407A"},
+    {order : 4, title:"데코레이션", color:"#D81B60"},
+    {order : 5, title:"케이크 판 꾸미기", color:"#C2185B"},
+];
+let active_order_item = document.querySelector('.order_num_item_active');
+let cur_order = order_list[0];
+
+order_num_items.forEach(element => {
+    element.addEventListener('click',()=>{
+        active_order_item.classList.toggle('order_num_item_active', false);
+        element.classList.toggle('order_num_item_active', true);
+        active_order_item = element;
+
+        cur_order = order_list[Number(active_order_item.dataset.order)-1];
+        order_title.innerText = cur_order.title;
+        order_title.style.backgroundColor = cur_order.color;
+
+        step_boxes.forEach(ele => {
+            if(ele.dataset.step == cur_order.order){
+                active_step_box.classList.toggle('unactive', true);
+                active_step_box.classList.toggle('active_step_box', false);
+                
+                ele.classList.toggle('unactive', false);
+                ele.classList.toggle('active_step_box', true);
+                active_step_box = ele;
+            }
+        });
+    });
+});
+
+/*--------------------------------- STEP2 --------------------------------- */
